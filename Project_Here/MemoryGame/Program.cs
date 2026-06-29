04faf7041de4b7e4bc3039b1c497e4d0200d9a69
katpium <uwuing88@gmail.com>
2026-06-29 14:42:22 +0700

    Create a new project
    a memory Game using C#, Json, CSS, HTML, JavaScript

---
+++ Project_Here/MemoryGame/Program.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics.Contracts;
+using System.IO.Pipelines;
+using System.Net.Http.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("api/cats", async () =>
+{
+    HttpClient client = new HttpClient();
+
+    //To get more images from the API I need to use the API key! 
+    //This is how to fetch it
+    client.DefaultRequestHeaders.Add("x-api-key", "live_swqdRK6fmlQ40nO4EUpFAy0y8efjmLiIyA8jwQYf0QqBgQatERvdlXygGtYnvyUP");
+
+    string url = "https://api.thecatapi.com/v1/images/search?limit=20";
+
+    List<CatImage>? cats = await client.GetFromJsonAsync<List<CatImage>>(url);
+
+    if(cats == null || cats.Count == 0)
+    {
+        return Result.NotFound("No cat image found.");
+    }
+});
+
+app.Run();
+
+public class CatImage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = "";
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; } = "";
+
+    [JsonPropertyName("width")]
+    public int Width { get; set; }
+
+    [JsonPropertyName("height")]
+    public int Height { get; set; }
+}
---
+++ Project_Here/MemoryGame/wwwroot/app.js
@@ -0,0 +1,8 @@
+//Get HTML elements to JAVASCRIPT
+const gameBoard = document.getElementById("game-board");
+const statusText = document.getElementById("status");
+const restartButton = document.getElementById("restart-button");
+
+//Create a list for cards
+let cards = [];
+let firstCard = null;
